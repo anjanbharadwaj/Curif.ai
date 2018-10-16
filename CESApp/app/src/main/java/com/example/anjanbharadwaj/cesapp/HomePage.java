e3a31f088ce28f98e70ac509be05caaeb6871afe
anjanbharadwaj <anjanbharadwaj02@gmail.com>
2018-10-15 18:44:37 -0700

    Stashing changes

--- CESApp/app/src/main/java/com/example/anjanbharadwaj/cesapp/HomePage.java
+++ CESApp/app/src/main/java/com/example/anjanbharadwaj/cesapp/HomePage.java
@@ -14,8 +14,9 @@ import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -25,6 +26,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -36,9 +38,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ml.custom.FirebaseModelDataType;
 import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
 import com.google.firebase.ml.custom.FirebaseModelInputs;
@@ -47,27 +58,32 @@ import com.google.firebase.ml.custom.FirebaseModelManager;
 import com.google.firebase.ml.custom.FirebaseModelOptions;
 import com.google.firebase.ml.custom.FirebaseModelOutputs;
 import com.google.firebase.ml.custom.model.FirebaseLocalModelSource;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class HomePage extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    Toolbar toolbar;
+    FloatingSearchView searchView;
+    CoordinatorLayout coordinatorLayout;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference();
+    FloatingActionButton fab;
+    static ArrayList<Person> suggestions = new ArrayList<>();
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    final int VOICE_SEARCH_CODE = 3012;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private static final int NOTIFICATION_ID = 12345;
+
 
     private Context mContext=HomePage.this;
     private static final int REQUEST = 112;
@@ -89,22 +105,50 @@ public class HomePage extends AppCompatActivity {
         } else {
             //do here
         }
+        searchView = (FloatingSearchView) findViewById(R.id.searchView);
 
-
-
-
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        coordinatorLayout.bringToFront();
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //set adapter to your ViewPager
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+
+        //intialize the tab layout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //add 3 new tabs.
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //default tab that it loads on is the middle tab
+        viewPager.setCurrentItem(0);
+
+
+        //create page listener to return the tab at a certain position.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,9 +157,164 @@ public class HomePage extends AppCompatActivity {
                   //      .setAction("Action", null).show();
             }
         });
+        //set the behavior when the menu is clicked.
+        searchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+            @Override
+            public void onActionMenuItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.voice:
+                        // Voice search
+                        startVoiceRecognition();
+                        searchView.setSearchFocused(true);
+                        break;
+                    case R.id.feedback:
+                        //Send a bug report via email using email intent.
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/html");
+                        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"biblioflyfbla@gmail.com"});
+                        intent.putExtra(Intent.EXTRA_SUBJECT, "Bibliofly Bug Report");
+                        intent.putExtra(Intent.EXTRA_TEXT, "My bug...");
+
+                        startActivity(Intent.createChooser(intent, "Send Email"));
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(HomePage.this, "LOGOUT", Toast.LENGTH_LONG).show();
+                        //use our authentication database to sign out.
+                        FirebaseAuth auth = FirebaseAuth.getInstance();
+                        auth.signOut();
+                        //move user to sign in page once signed out.
+                        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(i);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        searchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+                updateSearches(newQuery);
+            }
+
+        });
+
+        searchView.setOnFocusChangeListener(new FloatingSearchView.OnFocusChangeListener() {
+            @Override
+            public void onFocus() {
+                updateSearches(searchView.getQuery());
+            }
+
+            @Override
+            public void onFocusCleared() {
+
+            }
+        });
+
+        //manages search query suggestions.
+        searchView.setOnBindSuggestionCallback(new SearchSuggestionsAdapter.OnBindSuggestionCallback() {
+            @Override
+            public void onBindSuggestion(View suggestionView, ImageView leftIcon, TextView textView, SearchSuggestion item, int itemPosition) {
+                String body = item.getBody();
+                String htmlText = body;
+                String query = searchView.getQuery();
+
+                if (query.length() == 0) return;
+
+                ArrayList<String> queryTokens = new ArrayList<>();
+
+                StringTokenizer st = new StringTokenizer(query);
+                while (st.hasMoreTokens()) {
+                    queryTokens.add(st.nextToken().toLowerCase());
+                }
+
+
+                for (String currQuery : queryTokens) {
+                    htmlText = htmlText.replaceAll("(?i)" + currQuery, "<font color=#999999>" + currQuery + "</font>");
+                }
+
+
+                textView.setText(Html.fromHtml(htmlText));
+            }
+
+        });
+
+        searchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(SearchSuggestion searchSuggestion) {
+                searchView.setSearchFocused(false);
+            }
+
+            @Override
+            public void onSearchAction(String currentQuery) {
+
+            }
+        });
+
+
 
     }
 
+
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private void updateSearches(String query) {
+
+        final String newQuery = query;
+        final DatabaseReference searchRef = database.getReference();
+
+        searchRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                suggestions.clear();
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    String isbn = d.getKey();
+                    String title = d.child("Title").getValue(String.class);
+                    String author = d.child("Author").getValue(String.class);
+                    String description = d.child("Description").getValue(String.class);
+                    double rating = d.child("Rating").getValue(Double.class);
+                    String url = d.child("URL").getValue(String.class);
+                    String lQuery = newQuery.toLowerCase();
+                    StringTokenizer st = new StringTokenizer(lQuery);
+
+                    boolean allTokens = false;
+
+                    while (st.hasMoreTokens()) {
+                        String currToken = st.nextToken();
+                        if (title.toLowerCase().contains(currToken) || author.toLowerCase().contains(currToken) || description.toLowerCase().contains(currToken)) {
+                            allTokens = true;
+                        } else {
+                            allTokens = false;
+                            break;
+                        }
+                    }
+
+                    if (allTokens) {
+                        suggestions.add(new Person(isbn, title, author, description, rating, url));
+                    }
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    //starts android voice recognition Intent.
+    public void startVoiceRecognition() {
+        Intent intent = new Intent("android.speech.action.RECOGNIZE_SPEECH");
+        intent.putExtra("android.speech.extra.LANGUAGE_MODEL", "free_form");
+        intent.putExtra("android.speech.extra.PROMPT", "Speak Now");
+        this.startActivityForResult(intent, VOICE_SEARCH_CODE);
+    }
 
     private static boolean hasPermissions(Context context, String... permissions) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
@@ -143,9 +342,9 @@ public class HomePage extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+         //   return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
@@ -180,7 +379,7 @@ public class HomePage extends AppCompatActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_home_page, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            textView.setText("Hi");
             return rootView;
         }
     }
@@ -234,7 +433,7 @@ public class HomePage extends AppCompatActivity {
                     Uri selectedImage = imageUri;
                     getContentResolver().notifyChange(selectedImage, null);
                     ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
+                    final Bitmap bitmap;
                     try {
                         bitmap = android.provider.MediaStore.Images.Media
                                 .getBitmap(cr, selectedImage);
@@ -307,8 +506,10 @@ public class HomePage extends AppCompatActivity {
                                                         float[][] output = result.<float[][]>getOutput(0);
                                                         float[] probabilities = output[0];
 
-                                                        System.out.println(Arrays.toString(probabilities));
+                                                        Log.e("PROBS",Arrays.toString(probabilities));
                                                         Toast.makeText(mContext, "Prediction Made!", Toast.LENGTH_LONG).show();
+                                                        saveData(probabilities,bitmap);
+
                                                     }
                                                 })
                                         .addOnFailureListener(
@@ -328,5 +529,91 @@ public class HomePage extends AppCompatActivity {
                 }
         }
     }
+    public void saveData(float[] probabilities, Bitmap bitmap){
+        final String time = ""+System.currentTimeMillis();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+
+        int maxIndex = 0;
+        for(int i = 0; i<probabilities.length; i++){
+            if(probabilities[i]>probabilities[maxIndex]){
+              maxIndex = i;
+            }
+            ref.child("Users").child(uid).child("Pictures").child(time).child("FullPredictions").child(""+(i+1)).setValue(probabilities[i]);
+        }
+        maxIndex+=1;
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("Pictures");
+        ref.child(time).child("Diagnosis").setValue(""+maxIndex);
+
+        final StorageReference imagesRef = FirebaseStorage.getInstance().getReference().child("Users").child(uid).child("Pictures").child(time);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = imagesRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //Toast.makeText(HomePage.this, "ON SUCCESS", Toast.LENGTH_SHORT).show();
+                taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.i("GOT URL", "VALUE IS" + String.valueOf(uri));
+                        ref.child(time).child("URL").setValue(uri);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                    }
+                });
+
+
+
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
+
+    }
+    private class PageAdapter extends FragmentPagerAdapter {
+
+        PageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new ProfileFragment();
+                default:
+                    return new PlaceholderFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Profile";
+                default:
+                    return "Placeholder";
+            }
+        }
+    }
+
+
 
 }
+
+
---
+++ CESApp/app/src/main/java/com/example/anjanbharadwaj/cesapp/ProfileFragment.java
@@ -0,0 +1,213 @@
+package com.example.anjanbharadwaj.cesapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+    This is the StudentTeacherProfile Fragment. It is the rightmost screen in the home page, and has information about the
+    current user, include their holds/checked out books and more.
+ */
+public class ProfileFragment extends Fragment {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    ListView listView;
+    ImageView share;
+    Context context;
+
+    ArrayList<DataPointProfile> listData = new ArrayList<>();
+
+    ArrayAdapter<DataPointProfile> dataPointProfileArrayAdapter;
+    //ArrayList<String> listData1 = new ArrayList<>();
+
+    public DatabaseReference database;
+    public static String name = "Profile";
+
+    int numCopies;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // do your variables initialisations here except Views!!!
+
+        context = getActivity().getApplicationContext();
+    }
+
+    public void loadData() {
+
+
+        //Clear our arraylists that hold the old data
+        listData.clear();
+
+        //Instantiate the two array adapters that connect the arraylists to listviews
+        dataPointProfileArrayAdapter = new DataPointProfileArrayAdapter(context, 0, listData);
+        database = FirebaseDatabase.getInstance().getReference();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        database.child("Users").child(uid).child("Pictures").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                listData.clear();
+                name = "Profile";//dataSnapshot.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Name").getValue().toString();
+
+                //Notify the adapters that the arraylists have changed, and that they have to update info
+
+                dataPointProfileArrayAdapter.notifyDataSetChanged();
+
+                listView.setAdapter(dataPointProfileArrayAdapter);
+
+                setListViewHeight(listView);
+
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+
+        });
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // initialize our views
+
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.profileSwipeRefresh);
+
+        listView = (ListView) view.findViewById(R.id.profileListView);
+        share = (ImageView) view.findViewById(R.id.share);
+
+        //loading
+
+        //loadData();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //loadData();
+            }
+        });
+
+        // Holds share button pressed
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    }
+
+    public static void setListViewHeight(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) return;
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+
+    }
+
+    public static String getName() {
+        return name;
+    }
+}
+
+//Wrapper class for book information.
+class DataPointProfile {
+    String url;
+    String diagnosis;
+    String date;
+
+    public DataPointProfile(String url, String diagnosis, String date) {
+        this.url = url;
+        this.diagnosis = diagnosis;
+        this.date = date;
+    }
+}
+
+//adapter which manages the data in the profile fragment list view.
+class DataPointProfileArrayAdapter extends ArrayAdapter<DataPointProfile> {
+
+    private Context context;
+    private List<DataPointProfile> dataList;
+
+    public DataPointProfileArrayAdapter(Context context, int resource, List<DataPointProfile> dataList) {
+        super(context, resource, dataList);
+
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //inflates a card and populates/adds the proper information
+
+        DataPointProfile dataPoint = dataList.get(position);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.profile_photo_taken, null);
+
+        //initializes the views on the card.
+        ImageView picture = (ImageView) view.findViewById(R.id.picture);
+        TextView diagnosis = (TextView) view.findViewById(R.id.diagnosis);
+        TextView date = (TextView) view.findViewById(R.id.date);
+
+        //loading book image async with Glide loading library.
+        Glide.with(context).load(dataPoint.url).into(picture);
+
+        //adding proper data to views.
+        diagnosis.setText(dataPoint.diagnosis);
+        date.setText(dataPoint.date);
+
+        return view;
+    }
+}
